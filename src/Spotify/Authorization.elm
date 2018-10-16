b292a61e8e19a2800a94b505913ef27fdbca6838
Ian Moore <ian.mark.moore@gmail.com>
2018-10-16 08:40:07 -0500

    auth happy path

--- src/Spotify/Authorization.elm
+++ src/Spotify/Authorization.elm
@@ -3,8 +3,8 @@ module Spotify.Authorization exposing (implicitAuthUrl)
 
 import Dict exposing (Dict)
 import List
+import Result
 import String
-import Tuple
 import Url exposing (Url)
 import Url.Builder as UrlBuilder
 import Url.Parser as UrlParser exposing ((</>))
@@ -58,45 +58,48 @@ fragmentParams frag =
     |> Dict.fromList
 
 
-buildAuthGrant : Dict String String -> GrantedData
-buildAuthGrant =
-    Dict.foldr
-        (\k v data ->
-            case k of
-                "auth_token" ->
-                    { data | accessToken = v }
-                _ ->
-                    data)
-        { accessToken = ""
-        , tokenType = ""
-        , expiresIn = 0
-        , state = ""
-        }
+authGrantFromParams : Dict String String -> Result String GrantedData
+authGrantFromParams params =
+    let
+        token = Dict.get "access_token" params
+        tokenType = Dict.get "token_type" params
+        expiresIn = Dict.get "expires_in" params
+        state = Dict.get "state" params
+    in
+        case [token, tokenType, expiresIn, state] of
+            [Just t1, Just t2, Just e, Just s] ->
+                case String.toInt e of
+                    Just x ->
+                        Ok <| GrantedData t1 t2 x s
+                    Nothing ->
+                        Err "Expiration time is invalid."
+            _ ->
+                Err "Response fragment does not contain all required parameters."
 
 
-
-fragmentAuthParser : Maybe String -> Result Authorization String
+fragmentAuthParser : Maybe String -> Result String Authorization
 fragmentAuthParser fragment =
     case fragment of
         Just f ->
             case String.contains "auth_token" f of
                 True ->
-                    fragmentParams f
-                    |> buildAuthGrant
-                    |> Granted
+                    fragmentParams f 
+                    |> authGrantFromParams 
+                    |> Result.map Granted
+                    |> Result.mapError (\s -> s ++ " Auth Response: " ++ f)
                 False ->
                     Err "WTF"
         Nothing ->
-            -- todo: parse denied query string
+            -- todo: parse denied query string via callbackParser oneOf
             Err "DENIED"
 
 
-callbackParser : String -> UrlParser.Parser (Result Authorization String -> a) a
+callbackParser : String -> UrlParser.Parser (Result String Authorization -> a) a
 callbackParser callbackPath =
     UrlParser.s callbackPath </> UrlParser.fragment fragmentAuthParser
 
 
-parseCallback : Url -> Result Authorization String 
+parseCallback : Url -> Result String Authorization
 parseCallback url =
     Err "TODO"
 
